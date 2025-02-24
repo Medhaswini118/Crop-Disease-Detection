581ab73d217779d4651321b6c11e1c00ed57fb35
Medhaswini <medhamedhaswini@gmail.com>
2025-02-24 20:31:14 +0530

    Update port

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if not set
-app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
